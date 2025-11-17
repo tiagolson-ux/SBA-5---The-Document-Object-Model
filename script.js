544d4e4147b64422fbbe48eaa0ce1a5b9deb22f1
Tia Noel <tiagolson@gmail.com>
2025-11-17 00:35:43 -0500

    step 2

--- script.js
+++ script.js
@@ -26,3 +26,41 @@ const STORAGE_KEY = "sba5_blog_posts_by_TN";
 console.log("Local storage key is:", STORAGE_KEY);
 // Note to self: commit after defining STORAGE_KEY and console.log
 
+// =======================
+// Step 2: Grab DOM elements
+// =======================
+
+// Notes to self:
+// I am getting references to HTML elements I will prolly use a lot.
+
+const postForm = document.querySelector("#postForm");
+const postTitleInput = document.querySelector("#postTitle");
+const postContentInput = document.querySelector("#postContent");
+const savePostButton = document.querySelector("#savePostButton");
+const cancelEditButton = document.querySelector("#cancelEditButton");
+const formHelper = document.querySelector("#formHelper");
+const postsContainer = document.querySelector("#postsContainer");
+const noPostsMessage = document.querySelector("#noPostsMessage");
+
+console.log("DOM elements selected:", {
+  postForm,
+  postTitleInput,
+  postContentInput,
+  savePostButton,
+  cancelEditButton,
+  formHelper,
+  postsContainer,
+  noPostsMessage,
+});
+// Note to self: commit after selecting DOM elements and logging them
+
+
+// Notes to self:
+// Helpe to get error message divs for title and content.
+const titleErrorDiv = document.querySelector('[data-error-for="title"]');
+const contentErrorDiv = document.querySelector('[data-error-for="content"]');
+
+console.log("Error message elements:", {
+  titleErrorDiv,
+  contentErrorDiv,
+});
